0cc579509be3a7907ca3511d410c3c87e5c1f7d6
roslanskyc <roslanskyc@gmail.com>
2022-05-09 10:51:09 -0500

    updated passwords.py

--- passwords/passwords.py
+++ passwords/passwords.py
@@ -1,3 +1,6 @@
+import hashlib
+import binascii
+
 
 f = open('words.txt')
 
@@ -8,5 +11,15 @@ hashed1 = [line.strip().lower() for line in open('passwords1.txt')]
 for x in range(len(hashed1)):
     hashed1[x] = hashed1[x].split(':')
 
-print(words[20])
-print(hashed1[20])
+for x in range(len(words)):
+    password = words[x].encode('utf-8')
+    hash = hashlib.sha256(password)
+    digest = hash.digest() 
+    digest_as_hex = binascii.hexlify(digest) 
+    digest_as_hex_string = digest_as_hex.decode('utf-8') # type=string
+    words[x] = [words[x], digest_as_hex_string]
+
+
+
+print(words[2000])
+print(hashed1[2110])
